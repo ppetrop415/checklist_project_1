9117a4b770ba280c3d5646a255eb381fd63745c1
ppetrop415 <ppetrop415@gmail.com>
2020-10-25 13:23:42 +0200

    settings update

--- checklist_project/settings.py
+++ checklist_project/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'checklist',
 ]
 
+CHOICES_SEPARATOR = ","
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
