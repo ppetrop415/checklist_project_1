01cd33b5d476fc4333c85d42a648c99e93a8989a
ppetrop415 <ppetrop415@gmail.com>
2020-10-25 13:07:06 +0200

    models update

--- business/models.py
+++ business/models.py
@@ -1,8 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
 from regions.models import Region, RegionUnity, State, ZipCode
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+class Activity(models.Model):
+    title = models.CharField(_("Title"), max_length=100)
+    slug = models.SlugField(blank=True)
+    order = models.PositiveSmallIntegerField(_("Display order"), blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = _("Activity")
+        verbose_name_plural = _("Activities")
+        ordering = ("order",)
+
+class TypeOfActivity(models.Model):
+    title = models.CharField(_("Title"), max_length=100)
+    slug = models.SlugField(blank=True)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = _("Type of Activity")
+        verbose_name_plural = _("Type of Activities")
 
 class Business(models.Model):
     title = models.CharField(max_length=100)
@@ -26,6 +53,8 @@ class BranchStore(models.Model):
     address = models.CharField(max_length=100)
     address_number = models.PositiveSmallIntegerField()
     email = models.EmailField(max_length=150)
+    # activity = models.ForeignKey(Activity, on_delete=models.SET_NULL, null=True, related_name="branchstores")
+    # type_of_activity = activity = models.ForeignKey(TypeOfActivity, on_delete=models.SET_NULL, null=True, related_name="branchstores")
 
     def __str__(self):
         return self.title
